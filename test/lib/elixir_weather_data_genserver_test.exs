79fb0f441ca885d6de4de049371545bb8a1a041f
guitarman <guitarman78@freenet.de>
2016-10-08 23:08:58 +0200

    refactoring

--- test/lib/elixir_weather_data_genserver_test.exs
+++ test/lib/elixir_weather_data_genserver_test.exs
@@ -8,7 +8,7 @@ defmodule ElixirWeatherDatai.GenServerTest do
         assert data[:centigrade] == 14.5
         #assert data[:created_at] == 1475699097
         assert data[:fahrenheit] == 58.1
-        assert data[:weather] == "überwiegend bewölkt"
+        assert data[:weather] == "cloudy"
         assert data[:wind_in_kilometers_per_hour] == 10.0
         assert data[:wind_in_meters_per_second] == 2.9
       {:error, reason} -> assert reason == :page_not_found
