e794c2964d5abe50c2759c71d0490cb9133e6e7f
guitarman <guitarman78@freenet.de>
2016-10-25 22:05:15 +0200

    api extension

--- test/lib/elixir_weather_data_genserver_test.exs
+++ test/lib/elixir_weather_data_genserver_test.exs
@@ -9,8 +9,13 @@ defmodule ElixirWeatherDatai.GenServerTest do
         #assert data[:created_at] == 1475699097
         assert data[:fahrenheit] == 58.1
         assert data[:weather] == "cloudy"
+        assert data[:humidity_in_percent] == 97
+        assert data[:icon] == "04n"
+        assert data[:icon_url] == "http://openweathermap.org/img/w/04n.png"
+        assert data[:pressure_in_hectopascal] == 997
         assert data[:wind_in_kilometers_per_hour] == 10.0
         assert data[:wind_in_meters_per_second] == 2.9
+        assert data[:wind_direction_in_degrees] == 246
       {:error, reason} -> assert reason == :page_not_found
     end
   end
